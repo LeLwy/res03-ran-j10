0548cb467c2b43b19e41064432a2b7c9e2fdf02d
LeLwy <Louis.Chancioux@gmail.com>
2022-11-21 15:46:00 +0100

    fin du js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,15 @@ let section = document.querySelectorAll("body > main > section");
 let fakeBtn = document.querySelectorAll("body > main > section > .fakeBtn");
 
 for (let i = 0; i < section.length; i++) {
-    
-    fakeBtn.addEventListener("click", function(e){
+    fakeBtn[i].addEventListener("click", function(){
+        
+        if((i+1) < section.length){
+            section[i].classList.remove("active");
+            section[i+1].classList.add("active");
+        }else{
+            section[i].classList.remove("active");
+            section[0].classList.add("active");
 
-        if(section[i].hasClass("active"))
+        }
     })    
 }
