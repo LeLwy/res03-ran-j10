7510f8841ca7e756707f09372d59b7279bcbe640
LeLwy <Louis.Chancioux@gmail.com>
2022-11-21 14:51:26 +0100

    début du JS

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+let section = document.querySelectorAll("body > main > section");
+let fakeBtn = document.querySelectorAll("body > main > section > .fakeBtn");
+
+for (let i = 0; i < section.length; i++) {
+    
+    fakeBtn.addEventListener("click", function(e){
+
+        if(section[i].hasClass("active"))
+    })    
+}
